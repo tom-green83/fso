522564dc33bac3d981bb4c9bc4e2ee18c21a76ac
wds <tom.green83@protonmail.com>
2022-01-09 17:41:23 +0000

    1.7 complete

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,17 +6,25 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+
+  const total = good + neutral + bad
+  const average = (good - bad) / total
+  const positive = 100 * good / total
+
+
   return (
     <div>
       <h1>give feedback</h1>
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>total {total}</p>
+      <p>average {average}</p>
+      <p>positive = {positive} %</p>
     </div>
   )
 }
