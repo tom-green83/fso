8022db12c9a89956681f5e9f76fa44dd7fc80134
wds <tom.green83@protonmail.com>
2022-01-10 20:30:11 +0000

    2.11 complete

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 
 const Person = ({ person }) => {
   return (
@@ -41,16 +42,20 @@ const Persons = ({ persons }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [persons, setPersons] = useState([])
 
+  // Get data from json-server
+  useEffect(() => {
+    axios.
+    get('http://localhost:3001/persons').
+    then(response => {
+      setPersons(response.data)
+    })
+  }, [])
+  
   const addName = (event) => {
     event.preventDefault()
     const nameExists = persons.find((person) => person.name === newName)
