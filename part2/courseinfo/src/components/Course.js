02424a85a63b100681eefa0fa4d1e60f09693839
wds <tom.green83@protonmail.com>
2022-01-10 14:06:03 +0000

    2.5 complete Courses.js in ./components is now utilised (previously unused in commits for 2.1 to 2.4 completion)

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,45 +1,50 @@
 import React from 'react';
 
+
 const Header = ({ course }) => {
-    return (
-      <h1>{course.name}</h1>
-    )
-  }
-  
-  const Total = ({ course }) => {
-    const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
-    return(
-      <p>Number of exercises {sum}</p>
-    ) 
-  }
-  
-  const Part = (props) => {
-    return (
-      <p>
-        {props.part.name} {props.part.exercises}
-      </p>    
-    )
-  }
-  
-  const Content = ({ course }) => {
-    return (
-      <div>
-        <Part part={course.parts[0]} />
-        <Part part={course.parts[1]} />
-        <Part part={course.parts[2]} />
-      </div>
-    )
-  }
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Total = ({ course }) => {
+  const parts = course.parts
+
+  // sum = reduce(reducer, initialValue)  //initialValue needed to pass first item through reducer
+  const reducer = (previousValue, currentValue) => previousValue + currentValue.exercises;
+  const sum = parts.reduce(reducer, 0)
+  return(
+    <p><strong>total of {sum} exercises</strong></p>
+  ) 
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>    
+  )
+}
+
+const Content = ({ course }) => {
+  const parts = course.parts
+  return (
+    <div>
+      {parts.map( part =>
+        <Part part={part} key={part.id} />
+      )}
+    </div>
+  )
+}
 
 const Course = ({ course }) => {
-    return (
-        <>
-            <Header course={course} />
-            <Content course={course} />
-            <Total course={course} />
-        </>
-    )
-
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
 }
 
 export default Course
--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,51 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Header = ({ course }) => {
-  return (
-    <h1>{course.name}</h1>
-  )
-}
-
-const Total = ({ course }) => {
-  const parts = course.parts
-
-  // sum = reduce(reducer, initialValue)  //initialValue needed to pass first item through reducer
-  const reducer = (previousValue, currentValue) => previousValue + currentValue.exercises;
-  const sum = parts.reduce(reducer, 0)
-  return(
-    <p><strong>total of {sum} exercises</strong></p>
-  ) 
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>    
-  )
-}
-
-const Content = ({ course }) => {
-  const parts = course.parts
-  return (
-    <div>
-      {parts.map( part =>
-        <Part part={part} key={part.id} />
-      )}
-    </div>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
