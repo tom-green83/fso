0c071bf88e2c7eeb8c92e8d3d565e250686d250a
wds <tom.green83@protonmail.com>
2022-01-14 18:40:30 +0000

    4.4 complete

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const initialValue = 0
+  const reducer = (previousValue, currentValue) => previousValue + currentValue.likes
+  return blogs.length === 0
+    ? 0
+    :blogs.reduce(reducer, initialValue)
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
