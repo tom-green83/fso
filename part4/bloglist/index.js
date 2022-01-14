2fc8a3d94582656c7a3efd4963b06b95b6c8f12a
wds <tom.green83@protonmail.com>
2022-01-14 17:56:43 +0000

    4.12 complete

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,49 +1,10 @@
-require('dotenv').config()
+const config = require('./utils/config')
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+const server = http.createServer(app)
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URL
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/', (request, response) => {
-  console.log('Homepage')
-  response.send('<h1>Homepage</h1>')
-})
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = process.env.PORT || 3003
-app.listen(PORT, () => {
+const PORT = config.PORT || 3003
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
