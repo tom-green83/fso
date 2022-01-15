0fbd401ba2c3ae20ae017af4cabef31d726b4e01
wds <tom.green83@protonmail.com>
2022-01-15 15:18:49 +0000

    4.8 complete

--- part4/bloglist/tests/bloglist_api.test.js
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -3,37 +3,28 @@ const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
+const helper = require('./test_helper')
 
 // Initialise remote DB
-const initialBlogs = [
-  {
-    _id: '5a422a851b54a676234d17f7',
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0
-  }
-]
 beforeEach (async () => {
   await Blog.deleteMany({})
-  initialBlogs.map(initialBlog => {
-    const blogObject = new Blog(initialBlog)
-    blogObject.save()
-  })
+  const blogObjects = helper.initialBlogs.map(initialBlog => new Blog(initialBlog))
+  const promiseArray = blogObjects.map(blogObject => blogObject.save())
+  await Promise.all(promiseArray)
 })
 
-test('correct number of blogs are returned as json', async () => {
-  const response = await api.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/)
-  expect(response.body).toHaveLength(initialBlogs.length)
+describe('get request to /api/blogs', () => {
+  test('returns blogs as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('returns correct number of blogs', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
 })
 
 afterAll(() => {
