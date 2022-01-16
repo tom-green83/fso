f8fd5ed028b53360fa679ccc5d22415fa3035ee2
wds <tom.green83@protonmail.com>
2022-01-16 19:56:30 +0000

    5.1 complete

---
+++ part5/bloglist-frontend/src/App.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [newBlog, setNewBlog] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null)
+  
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+    try {
+      const user = await loginService.login({username, password})
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    }
+    catch (exception) {
+      setErrorMessage('invalid credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const loginForm = () => (
+    <div>
+      <h2>login to application</h2>      
+      <form onSubmit={handleLogin}>
+        <div>
+          username <input type='text' value={username} name='Username' onChange={({target}) => setUsername(target.value)}/>
+        </div>
+        <div>
+          username <input type='password' value={password} name='Password' onChange={({target}) => setPassword(target.value)}/>
+        </div>
+        <button type='submit'>login</button>
+      </form>
+    </div>
+  )
+
+  const blogForm = () => (
+    <div>
+      <h2>blogs</h2>
+      <p>{user.name} logged in </p>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+
+  return(
+    <div>
+        {user === null
+          ? loginForm()
+          : blogForm()
+        }     
+    </div>
+  )
+}
+
+export default App
