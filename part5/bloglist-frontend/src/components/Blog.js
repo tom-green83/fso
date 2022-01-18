0673db9ade56fffa4379dbc20768b642ed02f4ec
wds <tom.green83@protonmail.com>
2022-01-18 19:38:03 +0000

    5.7 complete

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,36 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
+import React, { useState } from 'react'
+const Blog = ({blog}) => {
+  const [showDetails, setShowDetails] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const details = () => {
+    if (showDetails) {
+      return (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes}
+            <button>like</button>
+          </div>
+          <div>{blog.user.name}</div>
+        </div>          
+      )
+    }
+  }
+
+  return (
+    <div style ={blogStyle}>
     {blog.title} {blog.author}
-  </div>  
-)
+    <button onClick={() => setShowDetails(!showDetails)}>{showDetails ? 'hide' : 'show'}</button>
+    {details()}
+    </div>  
+  )
+}
 
 export default Blog
