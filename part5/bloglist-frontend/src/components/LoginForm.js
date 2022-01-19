90e6ceafaea17996180c63851d1d99faaa885df5
wds <tom.green83@protonmail.com>
2022-01-19 15:38:24 +0000

    Bugfix Remove "create new" heading on login page

--- part5/bloglist-frontend/src/components/LoginForm.js
+++ part5/bloglist-frontend/src/components/LoginForm.js
@@ -3,7 +3,6 @@ import React from 'react'
 const LoginForm = ( { handleLogin, username, setUsername, password, setPassword }) => {
   return(
     <>
-      <h2>create new</h2>
       <form onSubmit={handleLogin}>
         <div>
           username <input type='text' value={username} name='Username' onChange={({ target }) => setUsername(target.value)}/>
