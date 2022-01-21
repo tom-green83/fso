307d18bd94f2fe75f2804a1b885819bf37169c0e
wds <tom.green83@protonmail.com>
2022-01-21 19:03:32 +0000

    6.10 complete

---
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,11 @@
+const initialState = { text: 'default notification' }
+
+const notificationReducer = (state = initialState, action) => {
+  // console.log('state now: ', state)
+  // console.log('action', action)
+  switch (action.type){
+  default: return state
+  }
+}
+
+export default notificationReducer
