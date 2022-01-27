957b6c1c030f451aa8f0e6a0e32560214c5cf4dd
wds <tom.green83@protonmail.com>
2022-01-27 21:56:24 +0000

    8.5 complete

--- part8/library/library-backend/index.js
+++ part8/library/library-backend/index.js
@@ -105,7 +105,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!,
     authorCount: Int!,
-    allBooks(author: String): [Book]!,
+    allBooks(author: String, genre: String): [Book]!,
     allAuthors: [Author]!
   }
 
@@ -115,10 +115,14 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) {
-        return books
+      let filteredBooks = [...books]
+      if (args.author) {
+        filteredBooks = filteredBooks.filter(book => book.author===args.author)
       }
-      return books.filter(book => book.author===args.author)
+      if (args.genre) {
+        filteredBooks = filteredBooks.filter(book => book.genres.includes(args.genre))
+      }
+      return filteredBooks
     },
     allAuthors: () => authors
   },
