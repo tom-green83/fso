f73d336510ee13ee34dca43fd085e93026f347e0
wds <tom.green83@protonmail.com>
2022-01-28 19:17:53 +0000

    Cleanup

--- part8/library/library-frontend/src/components/Books.js
+++ part8/library/library-frontend/src/components/Books.js
@@ -4,7 +4,6 @@ import { useQuery } from '@apollo/client'
 
 const Books = (props) => {
   const result = useQuery(ALL_BOOKS_NO_GENRES)
-  console.log(result.data)
   if (result.loading) {
     return <div>loading...</div>
   }
