7fe4b88a1df4c021dfb1dde696144598bde02afc
wds <tom.green83@protonmail.com>
2022-01-30 14:13:34 +0000

    9.7 complete

--- part9/exercises/exerciseCalculator.ts
+++ part9/exercises/exerciseCalculator.ts
@@ -27,14 +27,14 @@ interface exerciseCalculatorResult {
   average: number;
 }
 
-const calculateExercises = (exerciseHours: Array<number>, target: number): exerciseCalculatorResult => {
+export const calculateExercises = (exerciseHours: Array<number>, target: number): exerciseCalculatorResult => {
   const periodLength = exerciseHours.length;
   const trainingDays = exerciseHours.filter(h => h > 0).length;
   const average = exerciseHours.reduce(function(a, b){return a + b;})/periodLength;
   const success = (average >= target);
 
   let rating, ratingDescription;
-  if (average < 0.5 * target) rating = 1, ratingDescription = 'needs a lot of work';
+  if (average < 0.5 * target) rating = 1, ratingDescription = 'bad';
   else if (average < target) rating = 2, ratingDescription = 'not too bad but could be better';
   else rating = 3, ratingDescription = 'excellent';
 
--- part9/exercises/index.ts
+++ part9/exercises/index.ts
@@ -1,16 +1,19 @@
 import express from 'express';
 const app = express();
 import { calculateBmi } from './bmiCalculator';
+import { calculateExercises } from './exerciseCalculator';
+
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
 
-app.get('/bmi', (_req, res) => {
-  const height = Number(_req.query.height);
-  const weight = Number(_req.query.weight);
+app.get('/bmi', (req, res) => {
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
   if (isNaN(height) || isNaN(weight)) {
-    res.status(404).json({error: 'malformatted parameters'});
+    res.status(400).json({error: 'malformatted parameters'});
   } else {
     res.send({
       weight: weight,
@@ -18,6 +21,25 @@ app.get('/bmi', (_req, res) => {
       bmi: calculateBmi(height, weight)
     });
   }
+});
+
+app.post('/exercises', (req, res) => {
+  try {
+    const daily_exercises = req.body.daily_exercises;
+    const target = req.body.target;
+    if (!daily_exercises || !target) {
+      throw new Error('parameters missing');
+    }
+    if (isNaN(Number(target))) throw new Error('malformatted parameters');
+    daily_exercises.map((e: any) => {
+      if (isNaN(Number(e))) throw new Error('malformatted parameters');
+    });
+    res.send(calculateExercises(daily_exercises, target));
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+
+
 });
 
 const PORT = 3002;
