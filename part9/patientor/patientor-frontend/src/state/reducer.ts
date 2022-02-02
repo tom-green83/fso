1a0de5739580d938c69a7824ad3e4acb0465b64f
wds <tom.green83@protonmail.com>
2022-02-02 18:06:05 +0000

    9.18 complete

--- part9/patientor/patientor-frontend/src/state/reducer.ts
+++ part9/patientor/patientor-frontend/src/state/reducer.ts
@@ -15,6 +15,27 @@ export type Action =
       payload: Patient;
     };
 
+export const setPatientList = (patientsFromApi: Patient[]): Action => {
+  return {
+    type: "SET_PATIENT_LIST",
+    payload: patientsFromApi
+  };
+};
+
+export const addPatient = (newPatient: Patient): Action => {
+  return {
+    type: "ADD_PATIENT",
+    payload: newPatient
+  };
+};
+
+export const setCurrentPatient = (patientFromApi: Patient): Action => {
+  return {
+    type: "SET_CURRENT_PATIENT",
+    payload: patientFromApi
+  };
+};
+
 export const reducer = (state: State, action: Action): State => {
   switch (action.type) {
     case "SET_PATIENT_LIST":
